5defd20e25053a7029a123771e7b26180d2af1fd
MD MASUD RANA <86624203+masud51435@users.noreply.github.com>
2024-06-07 23:51:11 +0600

    learn

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:googlemap/homepage.dart';
+import 'package:googlemap/map_learning/google_map_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,7 +19,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const GoogleMapPage(),
+      home: const GoogleMapEx1(),
     );
   }
 }
---
+++ lib/map_learning/google_map_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class GoogleMapEx1 extends StatefulWidget {
+  const GoogleMapEx1({super.key});
+
+  @override
+  State<GoogleMapEx1> createState() => _GoogleMapEx1State();
+}
+
+class _GoogleMapEx1State extends State<GoogleMapEx1> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
