8d8c067817a63092fbde48f0606282a167425881
MD MASUD RANA <86624203+masud51435@users.noreply.github.com>
2024-11-27 22:16:58 +0600

    map revision

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,107 +1,107 @@
-import 'dart:async';
-import 'package:flutter/material.dart';
-import 'package:flutter_polyline_points/flutter_polyline_points.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:googlemap/constants.dart';
-import 'package:location/location.dart';
+// import 'dart:async';
+// import 'package:flutter/material.dart';
+// import 'package:flutter_polyline_points/flutter_polyline_points.dart';
+// import 'package:google_maps_flutter/google_maps_flutter.dart';
+// import 'package:googlemap/constants.dart';
+// import 'package:location/location.dart';
 
-class GoogleMapPage extends StatefulWidget {
-  const GoogleMapPage({super.key});
+// class GoogleMapPage extends StatefulWidget {
+//   const GoogleMapPage({super.key});
 
-  @override
-  State<GoogleMapPage> createState() => _GoogleMapPage();
-}
+//   @override
+//   State<GoogleMapPage> createState() => _GoogleMapPage();
+// }
 
-class _GoogleMapPage extends State<GoogleMapPage> {
-  @override
-  void initState() {
-    // TODO: implement initState
-    getPolyPoints();
-    getCurrentLocation();
-    super.initState();
-  }
+// class _GoogleMapPage extends State<GoogleMapPage> {
+//   @override
+//   void initState() {
+//     // TODO: implement initState
+//     getPolyPoints();
+//     getCurrentLocation();
+//     super.initState();
+//   }
 
-  final Completer<GoogleMapController> _controller = Completer();
+//   final Completer<GoogleMapController> _controller = Completer();
 
-  static const LatLng sourceLocation = LatLng(37.33500926, -122.03272188);
-  static const LatLng destination = LatLng(37.33429383, -122.06600055);
+//   static const LatLng sourceLocation = LatLng(37.33500926, -122.03272188);
+//   static const LatLng destination = LatLng(37.33429383, -122.06600055);
 
-  LocationData? currentLocation;
+//   LocationData? currentLocation;
 
-  void getCurrentLocation() {
-    Location location = Location();
+//   void getCurrentLocation() {
+//     Location location = Location();
 
-    location.getLocation().then(
-      (location) {
-        currentLocation = location;
-      },
-    );
-  }
+//     location.getLocation().then(
+//       (location) {
+//         currentLocation = location;
+//       },
+//     );
+//   }
 
-  List<LatLng> polylineCordinate = [];
-  void getPolyPoints() async {
-    PolylinePoints polylinePoints = PolylinePoints();
-    PolylineResult result = await polylinePoints.getRouteBetweenCoordinates(
-      google_api_key,
-      PointLatLng(sourceLocation.latitude, sourceLocation.longitude),
-      PointLatLng(destination.latitude, destination.longitude),
-    );
-    if (result.points.isNotEmpty) {
-      for (PointLatLng point in result.points) {
-        polylineCordinate.add(
-          LatLng(
-            point.latitude,
-            point.longitude,
-          ),
-        );
-      }
-      setState(() {});
-    }
-  }
+//   List<LatLng> polylineCordinate = [];
+//   void getPolyPoints() async {
+//     PolylinePoints polylinePoints = PolylinePoints();
+//     PolylineResult result = await polylinePoints.getRouteBetweenCoordinates(
+//       googleApiKey:  google_api_key,
+//       PointLatLng(sourceLocation.latitude, sourceLocation.longitude),
+//       PointLatLng(destination.latitude, destination.longitude), request: null,
+//     );
+//     if (result.points.isNotEmpty) {
+//       for (PointLatLng point in result.points) {
+//         polylineCordinate.add(
+//           LatLng(
+//             point.latitude,
+//             point.longitude,
+//           ),
+//         );
+//       }
+//       setState(() {});
+//     }
+//   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: currentLocation == null
-          ? const Center(
-              child: CircularProgressIndicator(),
-            )
-          : GoogleMap(
-              compassEnabled: true,
-              mapType: MapType.hybrid,
-              initialCameraPosition: CameraPosition(
-                target: LatLng(
-                  currentLocation!.latitude!,
-                  currentLocation!.longitude!,
-                ),
-                zoom: 13,
-              ),
-              polylines: {
-                Polyline(
-                  polylineId: const PolylineId('route'),
-                  points: polylineCordinate,
-                  color: Colors.deepPurple,
-                  width: 6,
-                ),
-              },
-              markers: {
-                const Marker(
-                  markerId: MarkerId('source'),
-                  position: sourceLocation,
-                ),
-                Marker(
-                  markerId: const MarkerId('currentLocation'),
-                  position: LatLng(
-                    currentLocation!.latitude!,
-                    currentLocation!.longitude!,
-                  ),
-                ),
-                const Marker(
-                  markerId: MarkerId('destination'),
-                  position: destination,
-                ),
-              },
-            ),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: currentLocation == null
+//           ? const Center(
+//               child: CircularProgressIndicator(),
+//             )
+//           : GoogleMap(
+//               compassEnabled: true,
+//               mapType: MapType.hybrid,
+//               initialCameraPosition: CameraPosition(
+//                 target: LatLng(
+//                   currentLocation!.latitude!,
+//                   currentLocation!.longitude!,
+//                 ),
+//                 zoom: 13,
+//               ),
+//               polylines: {
+//                 Polyline(
+//                   polylineId: const PolylineId('route'),
+//                   points: polylineCordinate,
+//                   color: Colors.deepPurple,
+//                   width: 6,
+//                 ),
+//               },
+//               markers: {
+//                 const Marker(
+//                   markerId: MarkerId('source'),
+//                   position: sourceLocation,
+//                 ),
+//                 Marker(
+//                   markerId: const MarkerId('currentLocation'),
+//                   position: LatLng(
+//                     currentLocation!.latitude!,
+//                     currentLocation!.longitude!,
+//                   ),
+//                 ),
+//                 const Marker(
+//                   markerId: MarkerId('destination'),
+//                   position: destination,
+//                 ),
+//               },
+//             ),
+//     );
+//   }
+// }
--- lib/map_learning/google_map_page.dart
+++ lib/map_learning/google_map_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class GoogleMapEx1 extends StatefulWidget {
   const GoogleMapEx1({super.key});
@@ -10,6 +11,18 @@ class GoogleMapEx1 extends StatefulWidget {
 class _GoogleMapEx1State extends State<GoogleMapEx1> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Google Map'),
+        centerTitle: true,
+      ),
+      body: const GoogleMap(
+        mapType: MapType.hybrid,
+        initialCameraPosition: CameraPosition(
+          target: LatLng(37.7749, -122.4194), // San Francisco
+          zoom: 15,
+        ),
+      ),
+    );
   }
 }
