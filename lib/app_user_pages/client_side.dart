d3216effa247a0b2c713a1db59d35f9255185016
MD MASUD RANA <86624203+masud51435@users.noreply.github.com>
2024-12-02 21:18:46 +0600

    added delivery boy and order page ui

--- lib/app_user_pages/client_side.dart
+++ lib/app_user_pages/client_side.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class ClientSide extends StatelessWidget {
   const ClientSide({super.key});
@@ -6,7 +7,68 @@ class ClientSide extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Client side'),),
+      appBar: AppBar(
+        title: const Text('Order details'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.menu),
+            onPressed: () {
+              // Show side menu
+            },
+          ),
+          IconButton(
+            icon: const Icon(Icons.map),
+            onPressed: () {
+              // Show side menu
+            },
+          ),
+        ],
+      ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          children: [
+            TextFormField(
+              decoration: const InputDecoration(
+                labelText: 'Customer Name',
+              ),
+            ),
+            const SizedBox(height: 15),
+            TextFormField(
+              decoration: const InputDecoration(
+                labelText: 'Customer Phone',
+              ),
+            ),
+            const SizedBox(height: 15),
+            TextFormField(
+              decoration: const InputDecoration(
+                labelText: 'Customer Address',
+              ),
+            ),
+            const SizedBox(height: 15),
+            TextFormField(
+              decoration: const InputDecoration(
+                labelText: 'Customer Bill Amount',
+              ),
+            ),
+            const SizedBox(height: 25),
+            SizedBox(
+              height: 450,
+              width: double.infinity,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(20),
+                child: const GoogleMap(
+                  mapType: MapType.hybrid,
+                  initialCameraPosition: CameraPosition(
+                    target: LatLng(37.7749, -122.4194), // San Francisco
+                    zoom: 15,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/app_user_pages/delivery_boy_side.dart
+++ lib/app_user_pages/delivery_boy_side.dart
@@ -7,7 +7,71 @@ class DeliveryBoySide extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('delivery boy side'),
+        title: const Text('delivery boy App'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Image(
+              image: AssetImage('assets/images/delivery.png'),
+            ),
+            const SizedBox(height: 20),
+            const Text(
+              'Enter My Order ID:',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 5),
+            TextFormField(
+              decoration: const InputDecoration(
+                labelText: '1234',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            const SizedBox(height: 20),
+            const Text(
+              'Delivery Adders:',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 10),
+            const Text(
+              'Phone Number:',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 10),
+            const Text(
+              'Amount to collect:',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                FilledButton(
+                  onPressed: () {},
+                  child: const Text('show Location'),
+                ),
+                const SizedBox(width: 20),
+                FilledButton(
+                  onPressed: () {},
+                  style: FilledButton.styleFrom(backgroundColor: Colors.blue),
+                  child: const Text('start Delivery'),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
