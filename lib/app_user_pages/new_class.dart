d627aa6790385c97864bbb163f3448cca385cd54
MD MASUD RANA <86624203+masud51435@users.noreply.github.com>
2025-01-01 00:07:12 +0600

    new class

---
+++ lib/app_user_pages/new_class.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class NewClass extends StatefulWidget {
+  const NewClass({super.key});
+
+  @override
+  State<NewClass> createState() => _NewClassState();
+}
+
+class _NewClassState extends State<NewClass> {
+  int number = 0;
+  Future<void> _incrementCounter() async {
+    await Future.delayed(const Duration(seconds: 10));
+    setState(() {
+      number++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Text('Number: $number'),
+          ElevatedButton(
+            onPressed: _incrementCounter,
+            child: const Text('Increment'),
+          ),
+        ],
+      ),
+    );
+  }
+}
